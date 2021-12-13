1bd4dcbd24244ea434501b440dc27598b9cdf539
duyphamluanh <32034702+duyphamluanh@users.noreply.github.com>
2021-12-13 14:32:34 +0700

    Create grade_item.php

---
+++ grade_item.php
@@ -0,0 +1,19 @@
+```
+class grade_item extends grade_object {
+    /**
+     * DB Table (used by grade_object).
+     * @var string $table
+     */
+    public $table = 'grade_items';
+
+    /**
+     * Array of required table fields, must start with 'id'.
+     * @var array $required_fields
+     */
+        public $required_fields = array('id', 'courseid', 'categoryid', 'itemname','itemtype', 'itemmodule', 'iteminstance',
+                                 'itemnumber', 'iteminfo', 'idnumber', 'calculation', 'gradetype', 'grademax', 'grademin',
+                                 'scaleid', 'outcomeid', 'gradepass', 'multfactor', 'plusfactor', 'aggregationcoef',
+                                 'aggregationcoef2', 'sortorder', 'display', 'decimals', 'hidden', 'locked', 'locktime',
+                                 'needsupdate', 'weightoverride', 'timecreated', 'timemodified','elorating');//NhienTest 'elorating'
+
+```
